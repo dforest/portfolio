2faa4d783394d866037b46ab3cc75b8d8338914d
Keita Mori <keita011@gmail.com>
2021-11-29 16:34:05 +0900

    Fix bug

--- src/components/Gallery/components/Gallery.js
+++ src/components/Gallery/components/Gallery.js
@@ -1,48 +1,50 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { useStaticQuery, graphql } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
 import GalleryItem from './GalleryItem'
 
 const Gallery = () => {
-  const works = useStaticQuery(graphql`
-  {
-    allWorksJson {
-      nodes {
-        caption
-        date
-        description
-        id
-        tags
-        links
-        image {
-          childImageSharp {
-            fluid {
-              src
+  return (
+    <div>
+      <StaticQuery
+        query={graphql`
+        {
+          allWorksJson {
+            nodes {
+              caption
+              date
+              description
+              id
+              tags
+              links
+              image {
+                childImageSharp {
+                  fluid {
+                    src
+                  }
+                }
+              }
             }
           }
         }
-      }
-    }
-  }
-  `)
+        `}
+        render={works => works && (<div>
+          {works.allWorksJson.nodes.map((obj) => {
+          return (<GalleryItem
+            key={obj.id}
+            id={obj.id}
+            image={obj.image && obj.image.childImageSharp.fluid.src}
+            tags={obj.tags}
+            date={new Date(obj.date)}
+            caption={obj.caption}
+            description={obj.description}
+            links={obj.links}
+          />);
+          })}
+          </div>
+        )}
+      />
 
-  return (
-    <div>
-      {works && (<div>
-        {works.allWorksJson.nodes.map((obj) => {
-        return (<GalleryItem
-          key={obj.id}
-          id={obj.id}
-          image={obj.image && obj.image.childImageSharp.fluid.src}
-          tags={obj.tags}
-          date={new Date(obj.date)}
-          caption={obj.caption}
-          description={obj.description}
-          links={obj.links}
-        />);
-        })}
-        </div>
-      )}
     </div>
   )
 }
--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,43 +1,44 @@
 import React from 'react'
-import { useStaticQuery, graphql } from "gatsby"
+import { StaticQuery, graphql } from "gatsby"
 import hostName from './Common'
 
 const Posts = () => {
-  const posts = useStaticQuery(graphql`
-  {
-    allPostsJson {
-      nodes {
-        id
-        isoDate(formatString: "YYYY.MM.DD")
-        link
-        title
-      }
-    }
-  }
-  `)
-
   const faviconSrc = (hostname) => {
     return `https://www.google.com/s2/favicons?domain=${hostname}`
   }
 
   return (<div>
-    {posts && (<div>
-      {posts.allPostsJson.nodes.map((obj) => {
-        const host = hostName(obj.link)
-        return (<article key={obj.id} className="row 75% work-item">
-        <div className="12u date">
-          <p>{obj.isoDate}</p>
-        </div>
-        <div className="12u col">
-          <a href={obj.link}>
-            <p className="title">{obj.title}</p>
-            <p className="site">
-              <img src={faviconSrc(host)} alt={host} />{host}
-            </p>
-          </a>
-        </div>
-      </article>)})}
-    </div>)}
+    <StaticQuery
+      query={graphql`
+      {
+        allPostsJson {
+          nodes {
+            id
+            isoDate(formatString: "YYYY.MM.DD")
+            link
+            title
+          }
+        }
+      }
+      `}
+      render={posts=> posts && (<div>
+        {posts.allPostsJson.nodes.map((obj) => {
+          const host = hostName(obj.link)
+          return (<article key={obj.id} className="row 75% work-item">
+          <div className="12u date">
+            <p>{obj.isoDate}</p>
+          </div>
+          <div className="12u col">
+            <a href={obj.link}>
+              <p className="title">{obj.title}</p>
+              <p className="site">
+                <img src={faviconSrc(host)} alt={host} />{host}
+              </p>
+            </a>
+          </div>
+        </article>)})}
+      </div>)}
+    />
   </div>)
 }
 
